b2ac1987e6aeb91771ccfd6718409fad72f2babe
veganafro <jmuhia80@gmail.com>
2016-11-16 17:27:58 -0500

    first commit

---
+++ iOS Programming 16 Nov - App Activity/BoxView.h
@@ -0,0 +1,13 @@
+//
+//  BoxView.h
+//  iOS Programming 16 Nov - App Activity
+//
+//  Created by Jeremia Muhia on 11/16/16.
+//  Copyright © 2016 NYU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BoxView : UIView
+
+@end
---
+++ iOS Programming 16 Nov - App Activity/BoxView.m
@@ -0,0 +1,21 @@
+//
+//  BoxView.m
+//  iOS Programming 16 Nov - App Activity
+//
+//  Created by Jeremia Muhia on 11/16/16.
+//  Copyright © 2016 NYU. All rights reserved.
+//
+
+#import "BoxView.h"
+
+@implementation BoxView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
